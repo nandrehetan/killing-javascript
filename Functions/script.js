f231b7e1cd00afe82bf494e3860c55cfac5dd7c0
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-12-08 19:16:29 +0530

    Update script.js

--- Functions/script.js
+++ Functions/script.js
@@ -8,3 +8,23 @@ age=15
 hetu(age)
 age=76
 hetu(age)
+
+function isodd( n){
+    if(n%2==0){
+        console.log(n+' is even')
+    }else{
+        console.log(n+' is odd')
+    }
+}
+isodd(56)
+isodd(75)
+
+function larger(num1,num2){
+    if(num1>num2){
+        console.log(num1+' is larger than '+num2)
+    }else{
+        console.log(num2+' is larger than '+num1)
+    }
+}
+larger(4,5)
+larger(7,6)
