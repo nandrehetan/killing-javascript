5d78f4c07f1f67bebf46d9e6731519824191ed06
Hetan Nandre <97376783+nandrehetan@users.noreply.github.com>
2024-12-08 21:59:47 +0530

    Add files via upload

---
+++ Objects/script.js
@@ -0,0 +1,16 @@
+let prod = {
+    name:'hetan',
+    father_name:'hemant',
+    surname:'nandre',
+    age:20,
+}
+
+console.log(prod)
+console.log(prod.name)
+console.log(prod.father_name)
+console.log(prod.surname)
+console.log(prod.age)
+
+
+prod.age=100
+console.log(prod.age)
